e9516bb693f9aa01d4d4fc32fb96be3513dce56f
RangersonTI <rangerson14@outlook.com>
2024-05-11 16:20:43 -0300

    .

--- NextFit.py
+++ NextFit.py
@@ -1,30 +1,26 @@
-def NextFit(bloco, processo, tam_bloco, tam_processo):
-    alocacao = [-1] * tam_processo  # Inicializando a lista alocacao com -1
-    
-    for i in range(tam_processo):
-        j = 0  # Reinicializa j a cada iteração do loop externo
-        while (j < tam_bloco):
-            if bloco[j] >= processo[i]:
-                alocacao[i] = j
-                bloco[j] -= processo[i]
-                break
-            j = (j+1) % tam_bloco
+
+class nextFit:
+    def NextFit(bloco, processo, tam_bloco, tam_processo):
+        alocacao = [-1] * tam_processo  # Inicializando a lista alocacao com -1
         
-    print("\nProcesso No. \tProcesso Size \tBlock no.\n", end="")
-    
-    for i in range(tam_processo):
-        print(str(i+1) + "\t\t" + str(processo[i]) + "\t\t", end="")
-        
-        if (alocacao[i] != -1):
-            print(str(alocacao[i] + 1))
-        else:
-            print("Não alocado", end="")
+        for i in range(tam_processo):
+            j = 0  # Reinicializa j a cada iteração do loop externo
+            while (j < tam_bloco):
+                if bloco[j] >= processo[i]:
+                    alocacao[i] = j
+                    bloco[j] -= processo[i]
+                    break
+                j = (j+1)
+                print(j)
             
-        print("")
-
-bloco = [5, 10, 20]  # Corrigido
-processo = [40, 10, 5]  # Corrigido
-tam_bloco = len(bloco)
-tam_processo = len(processo)
-
-NextFit(bloco, processo, tam_bloco, tam_processo)
+        print("\nN° do Processo.\tTam. Processo \tN° do Bloco.\n", end="")
+        
+        for i in range(tam_processo):
+            print(str(i+1) + "\t\t" + str(processo[i]) + "\t\t", end="")
+            
+            if (alocacao[i] != -1):
+                print(str(alocacao[i] + 1))
+            else:
+                print("Não alocado", end="")
+                
+            print("")
